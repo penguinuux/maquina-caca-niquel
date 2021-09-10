eeeeb25731ac59472a843875b526811db1456990
Pablo Adriano <pabloadrianomc@gmail.com>
2021-09-10 17:26:56 -0300

    refatorado função changeSlotImg permitindo a remoção do segundo botão

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,7 @@
 const slot1 = document.getElementById('slot1');
 const slot2 = document.getElementById('slot2');
 const slot3 = document.getElementById('slot3');
+let repeat = true;
 
 // gera números aleatórios (1 a 6)
 randomNumberGen = () => {
@@ -15,23 +16,35 @@ randomNumberGen = () => {
 
 // altera a imagem nos slots
 changeSlotImg = () => {
-    let randomChoose = setTimeout(() => {
-        let random1 = randomNumberGen();
-        let random2 = randomNumberGen();
-        let random3 = randomNumberGen();
+    let random1 = 0;
+    let random2 = 0;
+    let random3 = 0;
+
+    let interval = setInterval(() => {
+
+        random1 = randomNumberGen();
+        random2 = randomNumberGen();
+        random3 = randomNumberGen();
         
         slot1.src = `./assets/img/${random1}.png`;
         slot2.src = `./assets/img/${random2}.png`;
         slot3.src = `./assets/img/${random3}.png`;
 
-    }, 200);
+        if (repeat === true) {
+            clearInterval(interval);
+        }
 
+        return (random1, random2, random3);
+    }, 150);
+
+    console.log(random1, random2, random3);
 };
 
 
-clicked = () => {
+started = () => {
+    repeat = !repeat;
     changeSlotImg();
 }
 
-const button = document.getElementById('buttonStartStop');
-button.addEventListener('click', clicked);
+const buttonStartStop = document.getElementById('buttonStartStop');
+buttonStartStop.addEventListener('click', started);
