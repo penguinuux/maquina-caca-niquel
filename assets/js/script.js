4207acc165fd7791b023c9797bee924956f32223
Pablo Adriano <pabloadrianomc@gmail.com>
2021-09-11 09:46:11 -0300

    user messages condition added

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,31 +20,60 @@ changeSlotImg = () => {
     let random2 = 0;
     let random3 = 0;
 
+
     let interval = setInterval(() => {
 
+        const userMessage = document.getElementById('userMessage');
         random1 = randomNumberGen();
         random2 = randomNumberGen();
         random3 = randomNumberGen();
+
+        let lastRandom1 = '';
+        let lastRandom2 = '';
+        let lastRandom3 = '';
         
         slot1.src = `./assets/img/${random1}.png`;
         slot2.src = `./assets/img/${random2}.png`;
         slot3.src = `./assets/img/${random3}.png`;
 
-        if (repeat === true) {
+        userMessage.innerText = '';
+
+        if (repeat) {
             clearInterval(interval);
+            
+            lastRandom1 = random1;
+            lastRandom2 = random2;
+            lastRandom3 = random3;
+
+            // Messagens para o usuário
+            if (lastRandom1 === 1 && lastRandom2 === 2 && lastRandom3 === 3) {
+                const span = document.createElement('span');
+                span.innerHTML = '<b>7 7 7</b> Pessoa de sorte! Você ganhou o maior prêmio!';
+                userMessage.appendChild(span);
+
+            } else if (lastRandom1 === 8 && lastRandom2 === 8 && lastRandom3 === 8) {
+                const span = document.createElement('span');
+                span.innerHTML = '<b>JackPot</b> Achou o pote de dinheiro!' 
+                userMessage.appendChild(span);
+
+            } else if(lastRandom1 === lastRandom2 && lastRandom2 === lastRandom3) { 
+                const span = document.createElement('span');
+                span.innerHTML = '<b>TRIPLE!</b> Encha seus bolsos!' 
+                userMessage.appendChild(span);
+
+            } else if (lastRandom1 === lastRandom2 || lastRandom1 === lastRandom3 || lastRandom2 === lastRandom3) {
+                const span = document.createElement('span');
+                span.innerHTML = '<b>DOUBLE!</b> Você ganhou o segundo prêmio!';
+                userMessage.appendChild(span);
+            }
         }
+    }, 80);
+}
 
-        return (random1, random2, random3);
-    }, 150);
-
-    console.log(random1, random2, random3);
-};
-
-
-started = () => {
+clicked = () => {
     repeat = !repeat;
     changeSlotImg();
 }
 
 const buttonStartStop = document.getElementById('buttonStartStop');
-buttonStartStop.addEventListener('click', started);
+buttonStartStop.addEventListener('click', clicked);
